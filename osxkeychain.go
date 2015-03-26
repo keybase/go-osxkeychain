89f9c9d4a2037fc0fe15e23d4dac751985902fe1
Frederick Akalin <akalin@gmail.com>
2015-03-26 13:07:02 -0400

    Remove unused functions

--- osxkeychain.go
+++ osxkeychain.go
@@ -7,7 +7,6 @@ package osxkeychain
 #include <stdlib.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <Security/Security.h>
-#include "osxkeychain.h"
 */
 import "C"
 
@@ -16,43 +15,10 @@ import (
 	"unsafe"
 )
 
-type ProtocolType int
-
-const (
-	ProtocolHTTP ProtocolType = iota
-	ProtocolHTTPS
-	ProtocolAny
-)
-
-type AuthenticationType int
-
-const (
-	AuthenticationHTTPBasic AuthenticationType = iota
-	AuthenticationDefault
-	AuthenticationAny
-)
-
 type GenericPassword struct {
 	ServiceName string
 	AccountName string
 	Password    string
-}
-
-// A password for an Internet server, such as a Web or FTP server. Internet
-// password items on the keychain include attributes such as the security domain
-// and IP address.
-//
-// All string fields must have size that fits in 32 bits. All string
-// fields except for Password must be encoded in UTF-8.
-type InternetPassword struct {
-	ServerName     string
-	SecurityDomain string
-	AccountName    string
-	Path           string
-	Port           int // Use 0 to ignore
-	Password       string
-	Protocol       ProtocolType
-	AuthType       AuthenticationType
 }
 
 type _OSStatus C.OSStatus
@@ -90,58 +56,6 @@ func (ke *keychainError) Error() string {
 	return fmt.Sprintf("keychainError with unknown error code %d", ke.errCode)
 }
 
-func protocolTypeToC(t ProtocolType) (pt C.SecProtocolType) {
-	switch t {
-	case ProtocolHTTP:
-		pt = C.kSecProtocolTypeHTTP
-	case ProtocolHTTPS:
-		pt = C.kSecProtocolTypeHTTPS
-	default:
-		pt = C.kSecProtocolTypeAny
-	}
-	return
-}
-
-func protocolTypeToGo(proto C.CFTypeRef) ProtocolType {
-	if proto == nil {
-		// handle nil?
-		fmt.Println("nil proto in protocolTypeToGo")
-		return ProtocolAny
-	}
-	switch proto {
-	case C.kSecAttrProtocolHTTP:
-		return ProtocolHTTP
-	case C.kSecAttrProtocolHTTPS:
-		return ProtocolHTTPS
-	}
-	panic(fmt.Sprintf("unknown proto in protocolTypeToGo: %v", proto))
-}
-
-func authenticationTypeToC(t AuthenticationType) (at int) {
-	switch t {
-	case AuthenticationHTTPBasic:
-		at = C.kSecAuthenticationTypeHTTPBasic
-	case AuthenticationAny:
-		at = C.kSecAuthenticationTypeAny
-	default:
-		at = C.kSecAuthenticationTypeDefault
-	}
-	return
-}
-
-func authenticationTypeToGo(authtype C.CFTypeRef) AuthenticationType {
-	if authtype == nil {
-		// handle nil?
-		fmt.Println("nil authtype in authenticationTypeToGo")
-		return AuthenticationAny
-	}
-	switch authtype {
-	case C.kSecAttrAuthenticationTypeHTTPBasic:
-		return AuthenticationHTTPBasic
-	}
-	panic(fmt.Sprintf("unknown authtype in authenticationTypeToGo: %v", authtype))
-}
-
 func AddGenericPassword(pass *GenericPassword) error {
 	cpassword := C.CString(pass.Password)
 	defer C.free(unsafe.Pointer(cpassword))
@@ -156,59 +70,6 @@ func AddGenericPassword(pass *GenericPassword) error {
 		C.UInt32(len(pass.Password)),
 		unsafe.Pointer(cpassword),
 		&itemRef,
-	)
-	if errCode != C.noErr {
-		if err, exists := resultCodes[int(errCode)]; exists {
-			return err
-		}
-		return fmt.Errorf("Unmapped result code: %d", errCode)
-	}
-	defer C.CFRelease(C.CFTypeRef(itemRef))
-
-	return nil
-}
-
-// Adds an Internet password to the user's default keychain.
-func AddInternetPassword(pass *InternetPassword) error {
-	// TODO: Check fields for UTF-8 encoding and size fitting in
-	// 32 bits.
-
-	serverName := C.CString(pass.ServerName)
-	defer C.free(unsafe.Pointer(serverName))
-
-	// TODO: Make optional.
-	securityDomain := C.CString(pass.SecurityDomain)
-	defer C.free(unsafe.Pointer(securityDomain))
-
-	accountName := C.CString(pass.AccountName)
-	defer C.free(unsafe.Pointer(accountName))
-
-	path := C.CString(pass.Path)
-	defer C.free(unsafe.Pointer(path))
-
-	protocol := C.uint(protocolTypeToC(pass.Protocol))
-
-	authtype := C.uint(authenticationTypeToC(pass.AuthType))
-
-	password := unsafe.Pointer(C.CString(pass.Password))
-	defer C.free(password)
-
-	errCode := C.SecKeychainAddInternetPassword(
-		nil, // default keychain
-		C.UInt32(len(pass.ServerName)),
-		serverName,
-		C.UInt32(len(pass.SecurityDomain)),
-		securityDomain,
-		C.UInt32(len(pass.AccountName)),
-		accountName,
-		C.UInt32(len(pass.Path)),
-		path,
-		C.UInt16(pass.Port),
-		C.SecProtocolType(protocol),
-		C.SecAuthenticationType(authtype),
-		C.UInt32(len(pass.Password)),
-		password,
-		nil,
 	)
 
 	return newKeychainError(errCode)
@@ -231,11 +92,8 @@ func FindGenericPassword(pass *GenericPassword) (*GenericPassword, error) {
 		&itemRef,
 	)
 
-	if errCode != C.noErr {
-		if err, exists := resultCodes[int(errCode)]; exists {
-			return nil, err
-		}
-		return nil, fmt.Errorf("Unmapped result code: %d", errCode)
+	if ke := newKeychainError(errCode); ke != nil {
+		return nil, ke
 	}
 	defer C.CFRelease(C.CFTypeRef(itemRef))
 	defer C.SecKeychainItemFreeContent(nil, cpassword)
@@ -246,7 +104,7 @@ func FindGenericPassword(pass *GenericPassword) (*GenericPassword, error) {
 	return &resp, nil
 }
 
-func FindAndRemoveGenericPassword(pass *GenericPassword) (error) {
+func FindAndRemoveGenericPassword(pass *GenericPassword) error {
 	var itemRef C.SecKeychainItemRef
 
 	errCode := C.SecKeychainFindGenericPassword(
@@ -260,139 +118,14 @@ func FindAndRemoveGenericPassword(pass *GenericPassword) (error) {
 		&itemRef,
 	)
 
-	if errCode != C.noErr {
-		if err, exists := resultCodes[int(errCode)]; exists {
-			return err
-		}
-		return fmt.Errorf("Unmapped result code: %d", errCode)
+	if ke := newKeychainError(errCode); ke != nil {
+		return ke
 	}
+
 	defer C.CFRelease(C.CFTypeRef(itemRef))
 
 	errCode = C.SecKeychainItemDelete(itemRef)
-	if errCode != C.noErr {
-		if err, exists := resultCodes[int(errCode)]; exists {
-			return err
-		}
-		return fmt.Errorf("Unmapped result code: %d", errCode)
-	}
+	return newKeychainError(errCode)
 
 	return nil
-}
-
-// Finds the first Internet password item that matches the attributes you
-// provide in pass. Some attributes, such as ServerName and AccountName may be
-// left blank, in which case they will be ignored in the search.
-//
-// Returns an error if the lookup was unsuccessful.
-func FindInternetPassword(pass *InternetPassword) (*InternetPassword, error) {
-	// TODO: Check fields for UTF-8 encoding and size fitting in
-	// 32 bits.
-
-	serverName := C.CString(pass.ServerName)
-	defer C.free(unsafe.Pointer(serverName))
-
-	// TODO: Make optional.
-	securityDomain := C.CString(pass.SecurityDomain)
-	defer C.free(unsafe.Pointer(securityDomain))
-
-	accountName := C.CString(pass.AccountName)
-	defer C.free(unsafe.Pointer(accountName))
-
-	path := C.CString(pass.Path)
-	defer C.free(unsafe.Pointer(path))
-
-	protocol := C.uint(protocolTypeToC(pass.Protocol))
-
-	authtype := C.uint(authenticationTypeToC(pass.AuthType))
-
-	var passwordLength C.UInt32
-
-	var password unsafe.Pointer
-
-	var itemRef C.SecKeychainItemRef
-
-	errCode := C.SecKeychainFindInternetPassword(
-		nil, // default keychain
-		C.UInt32(len(pass.ServerName)),
-		serverName,
-		C.UInt32(len(pass.SecurityDomain)),
-		securityDomain,
-		C.UInt32(len(pass.AccountName)),
-		accountName,
-		C.UInt32(len(pass.Path)),
-		path,
-		C.UInt16(pass.Port),
-		C.SecProtocolType(protocol),
-		C.SecAuthenticationType(authtype),
-		&passwordLength,
-		&password,
-		&itemRef,
-	)
-
-	if err := newKeychainError(errCode); err != nil {
-		return nil, err
-	}
-
-	defer C.CFRelease(C.CFTypeRef(itemRef))
-	defer C.SecKeychainItemFreeContent(nil, password)
-
-	resp := InternetPassword{}
-	resp.Password = C.GoStringN((*C.char)(password), C.int(passwordLength))
-
-	// TODO: Audit the code below.
-
-	// Get remaining attributes
-	items := C.CFArrayCreateMutable(nil, 1, nil)
-	defer C.CFRelease(C.CFTypeRef(items))
-	C.CFArrayAppendValue(items, unsafe.Pointer(itemRef))
-	dict := C.CFDictionaryCreateMutable(nil, 0, nil, nil)
-	defer C.CFRelease(C.CFTypeRef(dict))
-	C.CFDictionaryAddValue(dict, unsafe.Pointer(C.kSecClass), unsafe.Pointer(C.kSecClassInternetPassword))
-	C.CFDictionaryAddValue(dict, unsafe.Pointer(C.kSecMatchItemList), unsafe.Pointer(items))
-	C.CFDictionaryAddValue(dict, unsafe.Pointer(C.kSecReturnAttributes), unsafe.Pointer(C.kCFBooleanTrue))
-
-	var result C.CFTypeRef = nil
-	errCode = C.SecItemCopyMatching(dict, &result)
-	if err := newKeychainError(errCode); err != nil {
-		return nil, err
-	}
-	defer C.CFRelease(result)
-
-	// get attributes out of attribute dictionary
-	resultdict := (C.CFDictionaryRef)(result) // type cast attribute dictionary
-	resp.AccountName = getCFDictValueString(resultdict, C.kSecAttrAccount)
-	resp.ServerName = getCFDictValueString(resultdict, C.kSecAttrServer)
-	resp.SecurityDomain = getCFDictValueString(resultdict, C.kSecAttrSecurityDomain)
-	resp.Path = getCFDictValueString(resultdict, C.kSecAttrPath)
-
-	resp.Protocol = protocolTypeToGo((C.CFTypeRef)(
-		C.CFDictionaryGetValue(resultdict, unsafe.Pointer(C.kSecAttrProtocol)),
-	))
-	resp.AuthType = authenticationTypeToGo((C.CFTypeRef)(
-		C.CFDictionaryGetValue(resultdict, unsafe.Pointer(C.kSecAttrAuthenticationType)),
-	))
-
-	// TODO: extract port number. The CFNumberRef in the dict doesn't appear to
-	// have a value.
-	// 	portref := (C.CFNumberRef)(C.CFDictionaryGetValue(dict, unsafe.Pointer(C.kSecAttrPort)))
-	// 	if portref != nil {
-	// 		var portval unsafe.Pointer
-	// 		portsuccess := C.CFNumberGetValue(portref, C.kCFNumberCharType, portval)
-	// 	}
-
-	return &resp, nil
-}
-
-func getCFDictValueString(dict C.CFDictionaryRef, key C.CFTypeRef) string {
-	if (int)(C.CFDictionaryGetCountOfKey(dict, unsafe.Pointer(key))) == 0 {
-		fmt.Println("dict doesn't contain key", key)
-	}
-	// maybe try CFDictionaryGetValueIfPresent to handle non-existent keys?
-	val := C.CFDictionaryGetValue(dict, unsafe.Pointer(key))
-	if val != nil {
-		valcstr := (*C.char)(C.CFStringGetCStringPtr((C.CFStringRef)(val), C.kCFStringEncodingUTF8))
-		defer C.CFRelease(C.CFTypeRef(valcstr))
-		return string(C.GoString(valcstr))
-	}
-	return ""
 }
--- osxkeychain_test.go
+++ osxkeychain_test.go
@@ -9,9 +9,9 @@ func TestGenericPassword(t *testing.T) {
 	accountNameVal := "bgentry"
 	serviceNameVal := "osxkeychain"
 	pass := GenericPassword{
-		ServiceName:     serviceNameVal,
-		AccountName:    accountNameVal,
-		Password: passwordVal,
+		ServiceName: serviceNameVal,
+		AccountName: accountNameVal,
+		Password:    passwordVal,
 	}
 	// Add the password
 	err := AddGenericPassword(&pass)
@@ -20,13 +20,13 @@ func TestGenericPassword(t *testing.T) {
 	}
 	// Try adding again, expect it to fail as a duplicate
 	err = AddGenericPassword(&pass)
-	if err != ErrDuplicateItem {
+	if ke, ok := err.(*keychainError); !ok || ke.getErrCode() != errDuplicateItem {
 		t.Errorf("expected ErrDuplicateItem on 2nd save, got %s", err)
 	}
 	// Find the password
 	pass2 := GenericPassword{
 		ServiceName: serviceNameVal,
-		AccountName:    accountNameVal,
+		AccountName: accountNameVal,
 	}
 	resp, err := FindGenericPassword(&pass2)
 	if err != nil {
@@ -45,69 +45,5 @@ func TestGenericPassword(t *testing.T) {
 	err = FindAndRemoveGenericPassword(&pass2)
 	if err != nil {
 		t.Error(err)
-	}
-}
-
-func TestInternetPassword(t *testing.T) {
-	passwordVal := "longfakepassword with \000 embedded nuls \000"
-	accountNameVal := "bgentry"
-	serverNameVal := "api.heroku.com"
-	securityDomainVal := ""
-	// 	portVal := 886
-	pathVal := "/fake"
-	pass := InternetPassword{
-		ServerName:     serverNameVal,
-		SecurityDomain: securityDomainVal,
-		AccountName:    accountNameVal,
-		// 		Port:           portVal,
-		Path:     pathVal,
-		Protocol: ProtocolHTTPS,
-		AuthType: AuthenticationHTTPBasic,
-		Password: passwordVal,
-	}
-	// Add the password
-	err := AddInternetPassword(&pass)
-	if err != nil {
-		t.Error(err)
-	}
-	// Try adding again, expect it to fail as a duplicate
-	err = AddInternetPassword(&pass)
-	if ke, ok := err.(*keychainError); !ok || ke.getErrCode() != errDuplicateItem {
-		t.Errorf("expected ErrDuplicateItem on 2nd save, got %s", err)
-	}
-	// Find the password
-	pass2 := InternetPassword{
-		ServerName: "api.heroku.com",
-		Path:       pathVal,
-		Protocol:   ProtocolHTTPS,
-		AuthType:   AuthenticationHTTPBasic,
-	}
-	resp, err := FindInternetPassword(&pass2)
-	if err != nil {
-		t.Error(err)
-	}
-	if resp.Password != passwordVal {
-		t.Errorf("FindInternetPassword expected Password=%s, got %s", passwordVal, resp.Password)
-	}
-	if resp.AccountName != accountNameVal {
-		t.Errorf("FindInternetPassword expected AccountName=%q, got %q", accountNameVal, resp.AccountName)
-	}
-	if resp.ServerName != serverNameVal {
-		t.Errorf("FindInternetPassword expected ServerName=%q, got %q", serverNameVal, resp.ServerName)
-	}
-	if resp.SecurityDomain != securityDomainVal {
-		t.Errorf("FindInternetPassword expected SecurityDomain=%q, got %q", securityDomainVal, resp.SecurityDomain)
-	}
-	if resp.Protocol != ProtocolHTTPS {
-		t.Errorf("FindInternetPassword expected Protocol=https, got %q", resp.Protocol)
-	}
-	// 	if resp.Port != portVal {
-	// 		t.Errorf("FindInternetPassword expected Port=%d, got %d", portVal, resp.Port)
-	// 	}
-	if resp.AuthType != AuthenticationHTTPBasic {
-		t.Errorf("FindInternetPassword expected AuthType=HTTPBasic, got %q", resp.AuthType)
-	}
-	if resp.Path != pathVal {
-		t.Errorf("FindInternetPassword expected Path=%q, got %q", pathVal, resp.Path)
 	}
 }
